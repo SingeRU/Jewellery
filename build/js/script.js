a9b18658885b2913a61cc138067b3007ce6c3819
ILIDAN <mrster03@gmail.com>
2022-04-08 04:51:16 +0300

    КатÐалог

--- build/js/script.js
+++ build/js/script.js
@@ -1,16 +1,44 @@
-let acc = document.getElementsByClassName("accordion");
+let body = document.querySelector('.catalog-body')
+let acc = document.getElementsByClassName('accordion');
+let filter = document.querySelector('.filter');
+let filterOpen = document.querySelector('.products-button--open');
+let filterClose = document.querySelector('.filter-button__close');
 
 for (let i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
-		this.classList.toggle("active");
+	acc[i].addEventListener('click', function() {
+		this.classList.toggle('active');
 		let panel = this.nextElementSibling;
 		if (panel.style.maxHeight){
 			panel.style.maxHeight = null;
 		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
+			panel.style.maxHeight = panel.scrollHeight + 'px';
 		} 
 	});
 }
+
+filterOpen.addEventListener('click', () => {
+	if(filter.classList.contains('opened')) {
+		filter.classList.remove('opened')
+		body.style.overflow = "visible";
+	} else {
+		filter.classList.add('opened')
+		body.style.overflow = 'hidden';
+	}
+});
+
+filterClose.addEventListener('click', () => {
+	filter.classList.remove('opened');
+	body.style.overflow = 'visible';
+})
+
+window.addEventListener('keydown', (evt) => {
+	if (evt.keyCode === 27) {
+	  if (filter.classList.contains('opened')) {
+		evt.preventDefault();
+		filter.classList.remove('opened');
+	  }
+	}
+  });
 
 const swiper = new Swiper('.swiper', {
 
