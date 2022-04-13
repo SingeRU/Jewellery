2fb0ea42fef9f8d1e9bfe1230019ea81ebf6ffc4
ILIDAN <mrster03@gmail.com>
2022-04-13 08:06:02 +0300

    Правки

--- build/js/script.js
+++ build/js/script.js
@@ -1,14 +1,21 @@
-let body = document.querySelector('.page-body')
+let body = document.querySelector('.page-body');
+let headerContainer = document.querySelector('.header')
 let acc = document.getElementsByClassName('accordion');
 let filter = document.querySelector('.filter');
 let filterOpen = document.querySelector('.products-open');
 let filterClose = document.querySelector('.filter-close');
 let login = document.querySelector('.login')
-let loginButton = document.querySelector('.options-login');
+let loginButtons = document.querySelectorAll('.options-login');
 let loginClose = document.querySelector('.login-close');
 let loginForm = document.querySelector('.login-form');
 let loginEmail = document.querySelector('.email');
 let loginPassword = document.querySelector('.password');
+let logo = document.querySelector('.header-top__logo__use');
+let burgerButton = document.querySelector('.header-burger');
+let burgerButtonSvg = document.querySelector('.header-burger__use')
+let optionsImage =document.querySelector('.options-image')
+let optionsImageSvg = document.querySelector('.options-image__use');
+let menu = document.querySelector('.menu');
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function() {
@@ -21,6 +28,30 @@ for (let i = 0; i < acc.length; i++) {
     }
   });
 }
+
+menu.classList.remove('menu--nojs')
+
+burgerButton.addEventListener('click', () => {
+  if(menu.classList.contains('menu--closed')) {
+    menu.classList.remove('menu--closed');
+    menu.classList.add('menu--opened');
+    headerContainer.style.backgroundColor = "#A87B62";
+    burgerButton.style.backgroundColor = "#A87B62";
+    optionsImage.style.backgroundColor = "#A87B62";
+    logo.style.fill = "#ffffff";
+    burgerButtonSvg.style.stroke = "#ffffff";
+    optionsImageSvg.style.stroke = "#ffffff";
+  } else {
+    menu.classList.add('menu--closed');
+    menu.classList.remove('menu--opened');
+    headerContainer.style.backgroundColor = "#ffffff";
+    burgerButton.style.backgroundColor = "#ffffff";
+    optionsImage.style.backgroundColor = "#ffffff";
+    logo.style.fill = "#A87B62";
+    burgerButtonSvg.style.stroke = "#1D1613";
+    optionsImageSvg.style.stroke = "#1D1613";
+  }
+});
 
 if(filterOpen != null) {
   filterOpen.addEventListener('click', () => {
@@ -41,12 +72,14 @@ if(filterClose != null) {
   });
 }
 
-loginButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  login.classList.remove('login--closed');
-  login.classList.add('login--opened');
-  body.style.overflow = 'hidden';
-});
+loginButtons.forEach((loginButton) => {
+  loginButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    login.classList.remove('login--closed');
+    login.classList.add('login--opened');
+    body.style.overflow = 'hidden';
+  });
+})
 
 loginClose.addEventListener('click', () => {
   loginClose.classList.remove('login--opened');
@@ -60,10 +93,11 @@ loginForm.addEventListener('submit', () => {
 })
 
 window.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 27) {
-    if (filter.classList.contains('opened')) {
+  if(login.classList.contains('login--opened')) {
     evt.preventDefault();
-    filter.classList.remove('opened');
-    }
+    login.classList.remove('login--opened');
+    login.classList.add('login--closed');
+    body.style.overflow = 'auto';
   }
+
 });
